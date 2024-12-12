dff6dda1b28281519fc5c6beb0e5147399b411a3
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-12-12 09:40:54 -0300

    Added and configured sliders.

--- front/src/swiper.js
+++ front/src/swiper.js
@@ -6,14 +6,14 @@ import './swiper.css'
 
 const banner = new Swiper('.swiper#banner', {
    loop: true,
+   speed: 750,
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
    },
    pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
+      clickable: true
    },
    autoplay: {
       delay: 7500,
@@ -23,13 +23,32 @@ const banner = new Swiper('.swiper#banner', {
 
 const promotions = new Swiper('.swiper#promotions', {
    loop: true,
-   slidesPerView: 5,
-   spaceBetween: 30,
-   slidesPerGroup: 5,
+   freeMode: true,
+   slidesPerView: 3,
+   spaceBetween: 20,
+   slidesPerGroup: 2,
+   speed: 1500,
    pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
+      clickable: true
+   },
+   autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      
+   }
+});
+
+const news = new Swiper('.swiper#news', {
+   loop: true,
+   freeMode: true,
+   slidesPerView: 5,
+   spaceBetween: 20,
+   slidesPerGroup: 5,
+   speed: 1500,
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true
    },
    autoplay: {
       delay: 5000,
